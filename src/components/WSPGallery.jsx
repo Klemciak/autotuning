8824020cd353e7ae6cfb55266e6f1b1cb41bdd16
Klemciak <klemcio@interia.pl>
2023-11-16 14:33:52 +0100

    Added gallery to lamborghini ProjectCar3

---
+++ src/components/WSPGallery.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react"
+import "../styles/carsStyles/wspGallery.scss"
+const WSPGallery = ({ galleryImages }) => {
+  const [slideNumber, setSlideNumber] = useState(0)
+  const [openModal, setOpenModal] = useState(false)
+
+  const handleOpenModal = (index) => {
+    setSlideNumber(index)
+    setOpenModal(true)
+  }
+  const handleCloseModal = () => {
+    setOpenModal(false)
+  }
+  const prevSlide = () => {
+    slideNumber === 0 ? setSlideNumber(galleryImages.length - 1) : setSlideNumber(slideNumber - 1)
+  }
+  const nextSlide = () => {
+    slideNumber + 1 === galleryImages.length ? setSlideNumber(0) : setSlideNumber(slideNumber + 1)
+  }
+  return (
+    <div>
+      {openModal && (
+        <div className="sliderWrap">
+          <span className="material-symbols-outlined btnClose" onClick={handleCloseModal}>
+            cancel
+          </span>
+          <span className="material-symbols-outlined btnPrev" onClick={prevSlide}>
+            arrow_circle_left
+          </span>
+          <span className="material-symbols-outlined btnNext" onClick={nextSlide}>
+            arrow_circle_right
+          </span>
+          <div className="fullScreenImage">
+            <img src={galleryImages[slideNumber].img} alt="" />
+          </div>
+        </div>
+      )}
+      <div className="galleryWrap">
+        {galleryImages &&
+          galleryImages.map((slide, index) => {
+            return (
+              <div className="single" key={index} onClick={() => handleOpenModal(index)}>
+                <img src={slide.img} alt="" />
+              </div>
+            )
+          })}
+      </div>
+    </div>
+  )
+}
+
+export default WSPGallery
--- src/pages/cars/ProjectCar3.js
+++ src/pages/cars/ProjectCar3.js
@@ -1,6 +1,58 @@
 import React from "react"
+import lamboImg1 from "../../graphics/lamborghini-aventador/lamborghini-aventador-img1.jpg"
+import lamboImg2 from "../../graphics/lamborghini-aventador/lamborghini-aventador-img2.jpg"
+import lamboImg3 from "../../graphics/lamborghini-aventador/lamborghini-aventador-img3.jpg"
+import lamboImg4 from "../../graphics/lamborghini-aventador/lamborghini-aventador-img4.jpg"
+import lamboImg5 from "../../graphics/lamborghini-aventador/lamborghini-aventador-img5.jpg"
+import lamboImg6 from "../../graphics/lamborghini-aventador/lamborghini-aventador-img6.jpg"
+import lamboImg7 from "../../graphics/lamborghini-aventador/lamborghini-aventador-img7.jpg"
+import lamboImg8 from "../../graphics/lamborghini-aventador/lamborghini-aventador-img8.jpg"
+import lamboImg9 from "../../graphics/lamborghini-aventador/lamborghini-aventador-img9.jpg"
+import lamboImg10 from "../../graphics/lamborghini-aventador/lamborghini-aventador-img10.jpg"
+import lamboImg11 from "../../graphics/lamborghini-aventador/lamborghini-aventador-img11.jpg"
+import WSPGallery from "../../components/WSPGallery"
+
 const ProjectCar3 = () => {
-  return <div>auto3</div>
+  const galleryImages = [
+    {
+      img: lamboImg1,
+    },
+    {
+      img: lamboImg2,
+    },
+    {
+      img: lamboImg3,
+    },
+    {
+      img: lamboImg4,
+    },
+    {
+      img: lamboImg5,
+    },
+    {
+      img: lamboImg6,
+    },
+    {
+      img: lamboImg7,
+    },
+    {
+      img: lamboImg8,
+    },
+    {
+      img: lamboImg9,
+    },
+    {
+      img: lamboImg10,
+    },
+    {
+      img: lamboImg11,
+    },
+  ]
+  return (
+    <div>
+      <WSPGallery galleryImages={galleryImages} />
+    </div>
+  )
 }
 
 export default ProjectCar3
