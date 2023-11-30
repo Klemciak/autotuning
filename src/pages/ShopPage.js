665e6e4d5057779d5da8f5a468f1e728b1d9d9f6
Klemciak <klemcio@interia.pl>
2023-11-30 17:01:30 +0100

    Added more products

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import "../styles/ShopPage.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons"
@@ -6,8 +6,29 @@ import storeItems from "../components/Shop/shopItems.json"
 import { StoreItem } from "../components/Shop/StoreItem.tsx"
 import { useShoppingCart } from "../components/Shop/ShoppingCartContext.tsx"
 import { ShoppingCart } from "../components/Shop/ShoppingCart.tsx"
+
+const itemsPerPage = 4
+
 const ShopPage = () => {
   const { openCart, cartQuantity } = useShoppingCart()
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentItems = storeItems.slice(indexOfFirstItem, indexOfLastItem)
+
+  const totalPages = Math.ceil(storeItems.length / itemsPerPage)
+
+  const handleClick = (type) => {
+    if (type === "prev" && currentPage > 1) {
+      setCurrentPage((numberPage) => numberPage - 1)
+    } else if (type === "next" && currentPage < totalPages) {
+      setCurrentPage((numberPage) => numberPage + 1)
+    } else if (typeof type === "number") {
+      setCurrentPage(type)
+    }
+  }
+
   return (
     <div className="shopWrapCart">
       {cartQuantity > 0 && (
@@ -21,11 +42,36 @@ const ShopPage = () => {
 
       <h1 className="shop_title">Sklep</h1>
       <div className="items">
-        {storeItems.map((item) => (
+        {currentItems.map((item) => (
           <div className="item" key={item.id}>
             <StoreItem {...item} />
           </div>
         ))}
+      </div>
+      <div className="shopWrapCart_paggination">
+        <button
+          onClick={() => handleClick("prev")}
+          disabled={currentPage === 1}
+          className="shopWrapCart_paggination_prevBtn"
+        >
+          Previous
+        </button>
+        {Array.from({ length: totalPages }, (i, index) => (
+          <button
+            key={index + 1}
+            onClick={() => handleClick(index + 1)}
+            className={`shopWrapCart_paggination_page ${currentPage === index + 1 ? "activePage" : ""}`}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button
+          onClick={() => handleClick("next")}
+          disabled={currentPage === totalPages}
+          className="shopWrapCart_paggination_nextBtn"
+        >
+          Next
+        </button>
       </div>
       <ShoppingCart />
       <p className="check">heh</p>
